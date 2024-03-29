4d5689755ce4aa8901bc3c22136cee5f3731fe59
cruz5203 <91767967+cruz5203@users.noreply.github.com>
2024-03-29 16:12:13 +0800

    智能合約

---
+++ InfoContract-master2/InfoContract.sol
@@ -0,0 +1,12 @@
+pragma  solidity  ^0.5.1;
+contract  InfoContract {
+    string  fName;
+    uint  age;
+    function  setInfo(string memory _fName, uint  _age) public {
+        fName =  _fName;
+        age =  _age;
+    }
+    function getInfo( ) public view returns (string memory, uint) {
+        return (fName, age);
+    }
+} 
